b86da34c4585b4be485210954a092d39039a50cf
mun3yy <braydenmcknight968@gmail.com>
2025-04-22 08:07:02 -0600

    Add files via upload

---
+++ public/games/placeholder.tsx
@@ -0,0 +1,9 @@
+// This is a placeholder component that will be replaced with actual game images
+// When you provide the list of games, we'll add the actual images to the public/games directory
+export default function GamePlaceholder() {
+  return (
+    <div className="flex items-center justify-center w-full h-full bg-zinc-800 text-zinc-400">
+      <span>Game Image</span>
+    </div>
+  )
+}
